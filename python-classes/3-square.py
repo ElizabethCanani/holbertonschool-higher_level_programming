ea1d27de00357dfb3a9646dd2a5f19ba393fba9f
ElizabethCanani <elicanani32@gmail.com>
2023-09-29 14:30:41 -0700

    Write a class Square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
-from sys import argv
+# 3-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Define a class Square."""
 
-if __name__ == '__main__':
-    ls = len(argv)
-    r = 0
 
-    if ls != 1:
-        for i in range(1, ls):
-            r += int(argv[i])
-    print("{0}".format(r))
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
