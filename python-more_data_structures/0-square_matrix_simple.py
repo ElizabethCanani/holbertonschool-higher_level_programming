8ac2e9dc033b01a3bff03cf2d69075d6fd3a7b6e
ElizabethCanani <elicanani32@gmail.com>
2023-09-29 18:53:33 -0700

    0. Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Crear una nueva matriz vacía del mismo tamaño que la matriz de entrada
     result = [[0] * len(row) for row in matrix]
 
-    # Iterar sobre cada elemento de la matriz de entrada
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            # Calcular el cuadrado del valor actual y asignarlo a la nueva matriz
             result[i][j] = matrix[i][j] ** 2
 
-    # Devolver la nueva matriz
     return result
