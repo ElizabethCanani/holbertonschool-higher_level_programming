2698b3dc6a68aaa0be4542192d9b6c2a9aa0b964
ElizabethCanani <elicanani32@gmail.com>
2023-09-29 19:58:38 -0700

    8. Safe function

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-# 101-safe_function.py
-# Brennan D Baraban <375@holbertonschool.com>
 
 import sys
+from typing import Any, Callable, Tuple
 
 
-def safe_function(fct, *args):
+def safe_function(fct: Callable[..., Any], *args: Tuple[Any]) -> Any:
     """Executes a function safely.
 
     Args:
@@ -18,7 +17,7 @@ def safe_function(fct, *args):
     """
     try:
         result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        return result
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
